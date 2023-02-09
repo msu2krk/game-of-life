77c7adf01e50582c1135e960ed7686b2e911a7c0
Michał Gajda <misiek.krk@gmail.com>
2023-02-09 10:13:00 +0100

    cells class dead/alive

--- main.js
+++ main.js
@@ -25,10 +25,25 @@ function createWorld() {
 		let tr = document.createElement("tr");
 		for (let j = 0; j < cols.value; j++) {
 			let cell = document.createElement("td");
+			cell.setAttribute("id", i + "_" + j);
+			cell.setAttribute("class", "dead");
+			cell.addEventListener("click", cellClick);
 
 			tr.appendChild(cell);
 		}
 		tbl.appendChild(tr);
 	}
 	world.appendChild(tbl);
+}
+
+function cellClick() {
+	let loc = this.id.split("_");
+	let row = Number(loc[0]); //Get i
+	let col = Number(loc[1]); //Get j
+	// Toggle cell alive or dead
+	if (this.className === "alive") {
+		this.setAttribute("class", "dead");
+	} else {
+		this.setAttribute("class", "alive");
+	}
 }
